4aba4f3d8a93e0889f0f6c723a028c5e161d5f05
Ben Gamari <ben@smart-cactus.org>
2019-07-31 14:36:36 -0400

    Add Show/Read instances

--- Expr.hs
+++ Expr.hs
@@ -5,7 +5,8 @@ data Expr
   | ELam Var Expr
   | EApp Expr Expr
   | ELit Integer
+  deriving (Read, Show)
 
 newtype Var = Var Int
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Read)
 
--- typed/Expr.hs
+++ typed/Expr.hs
@@ -5,13 +5,15 @@ data Type
   | TForall Var Type
   | TApp Type Type
   | TExpr Expr
+  deriving (Show, Read)
 
 data Expr
   = EVar Var Type
   | ELam Var Expr
   | EApp Expr Expr
   | ELit Integer
+  deriving (Show, Read)
 
 newtype Var = Var Int
-  deriving (Eq, Ord, Show)
+  deriving (Eq, Ord, Show, Read)
 
